96533785eb0435e6bd0c0288e0b349e142875a14
ywha0929 <ywha0929@gmail.com>
2022-11-05 21:26:58 +0900

    jk

--- Client/RunnableDistribution_Client/src/main/java/org/example/RunnableDistribution_Client.java
+++ Client/RunnableDistribution_Client/src/main/java/org/example/RunnableDistribution_Client.java
@@ -1,22 +1,24 @@
 package org.example;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class SingleDeviceTest {
+public class RunnableDistribution_Client {
+    static String[] ips;
+    static int[] ports;
+
+
     static int numThread = 4;
-
-
     public static void main(String[] args) {
-
-//        String nameTestFile = "../../TestFiles/TestFile.txt";
-        String nameTestFile = args[1];
+        int numThread = 4;
+        String nameTestFile = "../../TestFiles/TestFile.txt";
         File file = new File(nameTestFile);
         List<String> allLines;
         try {
@@ -46,11 +48,21 @@ public class SingleDeviceTest {
         ThreadController threadController = ThreadController.getInstance();
         threadController.setNumThread(numThread);
 
+        //initialize socket
+        for(int i= 0; i<ips.length; i++)
+        {
+            try{
+                Socket socket = new Socket(ips[i],ports[i]);
+            } catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
 
         for(int i = 0; i<listOperands.size(); i++)
         {
             int index = threadController.getIdleThreadIndex();
-            System.out.println("index : " + index);
+//            System.out.println("index : " + index);
             while(index == -1)
             {
                 index = threadController.getIdleThreadIndex();
@@ -65,3 +77,22 @@ public class SingleDeviceTest {
     }
 }
 
+class SocketOutputThread extends Thread {
+    String ip;
+    int port;
+    int index;
+    public SocketOutputThread(String ip, int port, int index)
+    {
+        this.ip = ip;
+        this.port = port;
+        this.index = index;
+    }
+    public void run() {
+        try{
+            Socket socket = new Socket(ip,port);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
