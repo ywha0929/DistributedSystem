8d0ec4c47844b5c48b4521c0d6deb662a8027130
ywha0929 <ywha0929@gmail.com>
2022-11-06 22:05:00 +0900

    done

--- SingleThreadTest/run.sh
+++ SingleThreadTest/run.sh
@@ -1,1 +1,1 @@
-java -jar SingleDeviceTest/out/artifacts/SingleDeviceTest_jar/SingleDeviceTest.jar --args 
+java -jar SingleDeviceTest/out/artifacts/SingleDeviceTest_jar/SingleDeviceTest.jar --args $1 
