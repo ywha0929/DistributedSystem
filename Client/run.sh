1f4e6d9504397cb7b56ff79c95f0a45ba4c11a98
ywha0929 <ywha0929@gmail.com>
2022-11-06 23:11:46 +0900

    12

--- Client/run.sh
+++ Client/run.sh
@@ -1,1 +1,1 @@
-java -jar RunnableDistribution_Client/out/artifacts/RunnableDistribution_Client_jar/RunnableDistribution_Client.jar --args $1 $2
+java -jar RunnableDistribution_Client/out/artifacts/RunnableDistribution_Client_jar/RunnableDistribution_Client.jar --args $1 $2 $3
