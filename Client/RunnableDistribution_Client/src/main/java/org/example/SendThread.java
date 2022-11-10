9ed65a7b1a2dd751511e0dbc1b53b5450b211f6b
ywha0929 <ywha0929@gmail.com>
2022-11-10 19:24:04 +0900

    fixing_multiserver

--- Client/RunnableDistribution_Client/src/main/java/org/example/SendThread.java
+++ Client/RunnableDistribution_Client/src/main/java/org/example/SendThread.java
@@ -2,30 +2,38 @@ package org.example;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SendThread extends Thread
 {
     OutputStream outputStream;
     DataOutputStream dataOutputStream;
     MessageQueue msgQ;
+    AtomicBoolean isQBusy;
     public SendThread(Socket socket) throws IOException {
         msgQ = new MessageQueue();
         outputStream =  socket.getOutputStream();
         dataOutputStream = new DataOutputStream(outputStream);
+        isQBusy = new AtomicBoolean(false);
     }
     public void putMsg(Message msg)
     {
+        while(isQBusy.get());
+        isQBusy.set(true);
         msgQ.pushMessage(msg);
+        isQBusy.set(false);
     }
 
     @Override
     public void run() {
         while (true)
         {
-            while (msgQ.msgQueue.isEmpty())
+            while (isQBusy.get() &&  msgQ.msgQueue.isEmpty())
                 for(int j = 0; j<5000; j++);
+            isQBusy.set(true);
             System.err.println("Send thread : Got Message");
             Message msg = msgQ.getMessage();
+            isQBusy.set(false);
             if(msg == null)
                 continue;
             try {
