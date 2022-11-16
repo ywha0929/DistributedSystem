d30a29a3063b566e886ed6c80e2e8001bd7adbee
ywha0929 <ywha0929@gmail.com>
2022-11-16 16:37:29 +0900

    1116

--- Client/RunnableDistribution_Client/src/main/java/org/example/RunnableDistribution_Client.java
+++ Client/RunnableDistribution_Client/src/main/java/org/example/RunnableDistribution_Client.java
@@ -14,7 +14,7 @@ public class RunnableDistribution_Client {
 //    static int[] ports;
     static Socket[] sockets;
     static int numServers = 1;
-    static int numThread = 4;
+    static int numThread = 8;
     static SendThread[] sendThreads;
     static ReadThread[] readThreads;
 
---
+++ SingleThreadTest/test.sh
@@ -0,0 +1,10 @@
+./run.sh 1 | tee SingleThread_1.txt
+./run.sh 1 | tee SingleThread_2.txt
+./run.sh 1 | tee SingleThread_3.txt
+./run.sh 1 | tee SingleThread_4.txt
+./run.sh 1 | tee SingleThread_5.txt
+./run.sh 10 | tee MultiThread_1.txt
+./run.sh 10 | tee MultiThread_2.txt
+./run.sh 10 | tee MultiThread_3.txt
+./run.sh 10 | tee MultiThread_4.txt
+./run.sh 10 | tee MultiThread_5.txt
